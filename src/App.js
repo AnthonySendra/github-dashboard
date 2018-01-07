5dbd8b3b604c1f997832b7621f07298f1ec9029c
Anthony Sendra <anthony.sendra@gmail.com>
2018-01-07 20:34:30 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-import { connect, Provider } from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './store'
 import Main from './containers/Main'
+import Reboot from 'material-ui/Reboot'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+
+const theme = createMuiTheme()
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <MuiThemeProvider theme={theme}>
+          <Reboot />
+          <Main />
+        </MuiThemeProvider>
       </Provider>
     )
   }
